8b8fdc4d5d72a67cba952cb499311f55eea6f255
TheHaruking <wta2m-c3@yahoo.co.jp>
2017-11-26 20:09:32 +0900

    整形

--- _171125_nunchuk_de_enso.ino
+++ _171125_nunchuk_de_enso.ino
@@ -1,87 +1,76 @@
-/*
- Fading
-
- This example shows how to fade an LED using the analogWrite() function.
-
- The circuit:
- * LED attached from digital pin 9 to ground.
-
- Created 1 Nov 2008
- By David A. Mellis
- modified 30 Aug 2011
- By Tom Igoe
-
- http://www.arduino.cc/en/Tutorial/Fading
-
- This example code is in the public domain.
-
- */
 #include "ArduinoNunchuk.h"
-// 赤: , 緑: , 白: , 黄: 
-
+// 緑: SDA(A4), 黄: SCL(A5)
 
 // ピン設定
-int trgPin   = 4;    // D:トリガー
+int trigPin  = 4;    // D:トリガー
 int cvPin    = 5;    // A:キーボード音程(980Hz)
 int pitchPin = 6;    // A:ピッチベンド, ビブラート
 int hpfPin   = 9;    // A:ハイパスフィルタ
 int lpfPin   = 10;   // A:ローパスフィルタ
 
 // ヌンチャク設定
-#define BAUDRATE 19200
 ArduinoNunchuk nunchuk = ArduinoNunchuk();
 
-// 音程テーブル
+// 音程テーブル (28音)
 int freq_tbl[] = {
-  51, 54, 57, 61,
+  51, 54, 57, 61,     // 1オクターブ目
   64, 68, 72, 76,
   81, 86, 91, 96,
-  102, 108, 114, 121,
+  102, 108, 114, 121, // 2オクターブ目
   129, 136, 144, 153,
   162, 172, 182, 193,
-  204, 216, 229, 243,
+  204, 216, 229, 243, // 3オクターブ目
 };
 
-// テスト用フレーズ(ドシドドシドソミ レシソﾌｧソシドシ)
-int doremi_tbl[] = {
-  0,  10, 12,  0, 10, 12,  7, 15,
-  14, 10,  7,  5,  7, 10, 12, 10,
+// テスト用フレーズ
+const int doremi_tbl[] = {
+  0,  10, 12,  0, 10, 12,  7, 15, // ドシドドシドソミ
+  14, 10,  7,  5,  7, 10, 12, 10, // レシソﾌｧソシドシ
 };
+const int doremi_len = sizeof(doremi_tbl) / sizeof(doremi_tbl[0]);
+
 // テスト用フレーズ用カウンタ
-const int count_max = 120;
 int count = 0;
+const int count_max = 12;
+int step = 0;   // 今、何番目のフレーズ音か
+
 
 void setup() {
+  // デジタルピン設定
+  pinMode(trigPin, OUTPUT);
   // ヌンチャク設定
-  Serial.begin(BAUDRATE);
   nunchuk.init();
 }
 
 void loop() {
+  // ヌンチャク取得
+  nunchuk.update();
 
-  // フレーズ長さ
-  int len = sizeof(doremi_tbl) / sizeof(doremi_tbl[0]);
+  // 計算
+  int trig = nunchuk.zButton;
+  int cv_freq = freq_tbl[ doremi_tbl[step] ];
+  int pitch_freq = nunchuk.analogY;
+  int hpf_freq = 0; // TODO
+  int lpf_freq = 0; // TODO
 
-  // fade in from min to max in increments of 5 points:
-  for (int i = 0; i < len; i++) {
-    nunchuk.update();
-    int freq = freq_tbl[ doremi_tbl[i] ];
-    // sets the value (range from 0 to 255):
-    analogWrite(cvPin, freq);   // 5
-    analogWrite(pitchPin, nunchuk.analogY);  // 6
-    // wait for 30 milliseconds to see the dimming effect
-    delay(120);
+  // 書込!
+  digitalWrite(trigPin,  trig);
+  analogWrite (cvPin,    cv_freq);
+  analogWrite (pitchPin, pitch_freq);
+  analogWrite (hpf_freq, hpf_freq);
+  analogWrite (lpf_freq, lpf_freq);
+
+  // 音長の処理
+  count++;
+  if (count >= count_max) {
+    step++;
+    count = 0;
   }
 
-  Serial.print(nunchuk.analogY, DEC);
-  Serial.print(' ');
-  // fade out from max to min in increments of 5 points:
-  //for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
-    // sets the value (range from 0 to 255):
-    // analogWrite(ledPin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    // delay(30);
-  //}
+  // フレーズ:最後まで到達したら戻す
+  if (step >= doremi_len) {
+    step = 0;
+  }
 }
 
 
